06e78da2af9ff743eec351665d0bfc494aec1a13
yellowmint <michnajacek0@gmail.com>
2020-09-21 19:47:50 +0200

    drf part 3

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from snippets import views
 
 urlpatterns = [
-    path('', views.snippet_list),
-    path('<int:pk>/', views.snippet_detail),
+    path('', views.SnippetList.as_view()),
+    path('<int:pk>/', views.SnippetDetail.as_view()),
 ]
